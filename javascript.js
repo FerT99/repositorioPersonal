42ec81b010870b93740f1d144ce4e22423ae27f8
Fer Torres <fertorres638@gmail.com>
2022-02-21 10:47:12 -0600

    laboratorio 1, 3

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function tabla(){
+    let ask = prompt("Ingresa un número: ");
+    for (let counter = 0; counter < ask.length(); counter++){
+
+        document.write('Resultado: ' + ask)
+        
+    }
+
+}
+
+
+
+
+
+
+class problema {
+    constructor() {
+
+    }
+}
