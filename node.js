351e454ba33c0721f1a889bfc28e4230bb3020ee
Fer Torres <fertorres638@gmail.com>
2022-02-28 11:36:54 -0600

    corrección de labs

---
+++ node.js
@@ -0,0 +1,35 @@
+/*
+
+const http = require("http");
+
+const server = http.createServer( (request, response) => {
+
+    if(request.url ==="/") {
+        console.log(request);
+        response.setHeader("Content-Type", "text/html");
+        response.write("<!DOCTYPE html><html lang = 'es-mx'><head><title>Caballos</title><meta charset = "utf/8"></meta></head>');
+        response.write('<body>');
+        response.write('<h1>Bienvenido</h1>');
+        response.write('<main>Aquí se habla de x</main>');
+        response.write("<ol>");
+        for (let i in caballos) {
+            response.write("<li> + caballos[i] + '</li>');
+        }
+        response.write('</body>');
+        response.end();
+
+} else {
+
+    response.statusCode = 404;
+    console.log(request);
+    response.setHeader("Content-Type", "text/html");
+    response.write("<!DOCTYPE html><html lang = 'es-mx'><head><title>Caballos | Page not found</title><meta charset = "utf/8"></meta></head>');
+    response.write('<body>');
+    response.write('<h1>Página "+ request.url +" no se encontró</h1>');
+    response.write('</body>');
+    response.end();
+}
+})
+*/
+//revisar que tipo de metodo http es, revisar info de ruta (url) y metodo (ejemplo: GET); 
+//para que no se cuelgue al mandar solicitud que no existe, se levanta un error
